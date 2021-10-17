f44f04ca3378b22f4541f07356a48723ada6699c
Pincette <68833587+Pincette1223@users.noreply.github.com>
2021-10-17 11:42:21 +0900

    데스메시지 추가 (사람이 죽었다.)

--- src/main/kotlin/kr/kro/pincette/kotlinpluginprac/KotlinPluginPrac.kt
+++ src/main/kotlin/kr/kro/pincette/kotlinpluginprac/KotlinPluginPrac.kt
@@ -1,16 +1,26 @@
-package kr.kro.pincette.kotlinpluginprac
+package kr.kro.`pincette-mcserver`.kotlinpluginprac
 
 import org.bukkit.Bukkit
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
+import net.md_5.bungee.api.ChatColor
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.PlayerDeathEvent
 
 class KotlinPluginPrac : JavaPlugin(),Listener {
     override fun onEnable() {
-        logger.info("Plugin Enable")
+        logger.info("${ChatColor.BLUE}플러그인이 활성화되었습니다.")
         Bukkit.getPluginManager().registerEvents(this, this)
     }
 
     override fun onDisable() {
-        logger.info("Plugin Disable")
+        logger.info("${ChatColor.RED}플러그인이 비활성화되었습니다.")
+    }
+
+    @EventHandler
+    fun onPlayerDeath(event: PlayerDeathEvent) {
+        event.deathMessage = null
+
+        event.deathMessage = "${ChatColor.RED}사람이 죽었다."
     }
 }
